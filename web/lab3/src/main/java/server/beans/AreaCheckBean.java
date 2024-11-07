8fb1f372fd12fe519f3d9dc35f679e33877a57da
Kostya666 <eliseevkonstantin306@gmail.com>
2024-11-07 15:56:39 +0300

    add result table

--- web/lab3/src/main/java/server/beans/AreaCheckBean.java
+++ web/lab3/src/main/java/server/beans/AreaCheckBean.java
@@ -1,100 +1,68 @@
 package server.beans;
 
-import jakarta.ejb.Stateless;
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.SessionScoped;
 import jakarta.inject.Named;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import lombok.Getter;
 import lombok.Setter;
+import server.models.Point;
+
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Logger;
 
 @Getter
 @Setter
-@Stateless
+@SessionScoped
 @Named("areaCheck")
 public class AreaCheckBean implements Serializable {
     private static final Logger logger = Logger.getLogger(AreaCheckBean.class.getName());
 
-//    @Inject
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private double x;
+    private double y;
+    private double r;
+    private boolean isHit;
+
+    private List<Integer> XValues = Arrays.asList(-3, -2, -1, 0, 1, 2, 3, 4, 5);
+    private List<Point> points;
+
     @PersistenceContext(unitName = "PointCheckerPU")
     private EntityManager em;
 
-    @Getter
-    @Setter
-    private double x;
-    @Getter
-    @Setter
-    private double y;
-    @Getter
-    @Setter
-    private double r;
-    private List<Integer> xValues = Arrays.asList(-3, -2, -1, 0, 1, 2, 3, 4, 5);
+    @PostConstruct
+    public void init() {
+        loadPoints();
+    }
 
-    public boolean checkHit(double x, double y, double r) {
+    private void loadPoints() {
+        points = em.createQuery("select p from Point p order by p.date", Point.class).getResultList();
+    }
+
+    @Transactional
+    private void addPoint(Point point) {
+        try {
+            em.persist(point);
+            loadPoints();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+    public boolean checkHit() {
+        Point point = new Point(x, y, r, isHit);
+        addPoint(point);
         return true;
     }
-
-    public List<Integer> getxValues() {
-        return xValues;
-    }
-
-
-    public void saveToDB() {
-        logger.info("x=" + x + ", y=" + y + ", r=" + r + ", isHit=" + true);
-        return;
-    }
-
-
-
-//    @Transactional
-//    public String saveToDB() {
-//        try {
-//            var isHit = checkHit(x, y, r);
-//            Point point = new Point(x, y, r, isHit);
-////            em.getTransaction().begin();
-//            logger.info("x=" + x + ", y=" + y + ", r=" + r + ", isHit=" + isHit);
-//            em.persist(point);
-////            em.merge(point);
-//            em.flush();
-//
-//
-//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Успех", "Данные сохранены."));
-//            logContextInfo();
-//        } catch (Exception e) {
-//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Ошибка", "Не удалось сохранить данные."));
-//            e.printStackTrace();
-//        }
-//        return null;
-//    }
-//
-//    public void logContextInfo() {
-//        FacesContext context = FacesContext.getCurrentInstance();
-//        testConnection();
-//        if (context != null) {
-//            // Логируем параметры запроса
-//            context.getExternalContext().getRequestParameterMap()
-//                    .forEach((key, value) -> logger.info("Request Parameter - " + key + ": " + value));
-//
-//            // Логируем атрибуты сессии
-//            context.getExternalContext().getSessionMap()
-//                    .forEach((key, value) -> logger.info("Session Attribute - " + key + ": " + value));
-//
-//            // Добавление тестового сообщения
-//            context.addMessage(null, new FacesMessage("Тестовое сообщение для отладки"));
-//        }
-//    }
-//
-//    public void testConnection() {
-//        try {
-//            this.em.createNativeQuery("SELECT 1").getSingleResult();
-////            System.out.println("Connection to database successful!");
-//            logger.info("Connection to database successful!");
-//        } catch (Exception e) {
-//            System.err.println("Failed to connect to the database: " + e.getMessage());
-//            e.printStackTrace();
-//        }
-//    }
 }
--- web/lab3/src/main/java/server/models/Point.java
+++ web/lab3/src/main/java/server/models/Point.java
@@ -1,57 +1,39 @@
 package server.models;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import java.io.Serializable;
+import java.util.Date;
 
-import java.time.LocalDate;
-
+@Getter
+@Setter
 @Entity
 @Table(name = "points")
-public class Point {
+public class Point implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false, unique = true)
     private Long id;
 
-    @NotNull
-    @Column(name = "X", nullable = false)
     private double x;
-
-    @NotNull
-    @Column(name = "Y", nullable = false)
     private double y;
-
-    @NotNull
-    @Column(name = "R", nullable = false)
     private double r;
 
-    @NotNull
-    @Column(name = "Hit", nullable = false)
+    @Column(name = "is_hit")
     private boolean isHit;
 
-    @NotNull
     @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "Date", nullable = false)
-    private LocalDate date;
+    private Date date;
 
-    public Point(double x, double y, double r, boolean isHit, LocalDate date) {
+    public Point() {}
+
+    public Point(double x, double y, double r, boolean isHit) {
         this.x = x;
         this.y = y;
         this.r = r;
-        this.isHit = isHit;
-        this.date = LocalDate.now();
+        this.date = new Date();
     }
 
-    public double getX() { return x; }
-    public double getY() { return y; }
-    public double getR() { return r; }
-    public boolean isHit() { return isHit; }
-    public LocalDate getDate() { return date; }
 
-    public void setX(double x) { this.x = x; }
-    public void setY(double y) { this.y = y; }
-    public void setR(double r) { this.r = r; }
-    public void setHit(boolean hit) { this.isHit = hit; }
-    public void setDate(LocalDate date) { this.date = date; }
 }
