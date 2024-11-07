0a2449dd5436c6eb5b527fdaac49ef628dc2afe6
Kostya666 <eliseevkonstantin306@gmail.com>
2024-11-07 17:18:25 +0300

    fix result table

--- web/lab3/src/main/java/server/beans/AreaCheckBean.java
+++ web/lab3/src/main/java/server/beans/AreaCheckBean.java
@@ -1,19 +1,20 @@
 package server.beans;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.inject.Named;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import lombok.Getter;
 import lombok.Setter;
 import server.models.Point;
 
-import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -24,45 +25,18 @@ import java.util.logging.Logger;
 public class AreaCheckBean implements Serializable {
     private static final Logger logger = Logger.getLogger(AreaCheckBean.class.getName());
 
-    @Serial
     private static final long serialVersionUID = 1L;
 
     private double x;
     private double y;
     private double r;
-    private boolean isHit;
 
     private List<Integer> XValues = Arrays.asList(-3, -2, -1, 0, 1, 2, 3, 4, 5);
-    private List<Point> points;
+    private List<Point> points = new ArrayList<>();
 
-    @PersistenceContext(unitName = "PointCheckerPU")
-    private EntityManager em;
-
-    @PostConstruct
-    public void init() {
-        loadPoints();
-    }
-
-    private void loadPoints() {
-        points = em.createQuery("select p from Point p order by p.date", Point.class).getResultList();
-    }
-
-    @Transactional
-    private void addPoint(Point point) {
-        try {
-            em.persist(point);
-            loadPoints();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-
-
-
-    public boolean checkHit() {
-        Point point = new Point(x, y, r, isHit);
-        addPoint(point);
-        return true;
+    public String submit() {
+        Point point = new Point(x, y, r);
+        points.add(point);
+        return null;
     }
 }
