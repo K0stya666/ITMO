9fb33d1f22484f16217d1009406e5e7e8b872aa0
Kostya666 <eliseevkonstantin306@gmail.com>
2024-11-10 17:49:14 +0300

    add new tests

--- web/lab3/src/test/java/HibernateExampleTest.java
+++ web/lab3/src/test/java/HibernateExampleTest.java
@@ -1,3 +1,4 @@
+import jakarta.persistence.TypedQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
@@ -7,6 +8,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import server.models.Point;
+
+import java.util.List;
 
 public class HibernateExampleTest {
     private SessionFactory sessionFactory;
@@ -36,33 +39,45 @@ public class HibernateExampleTest {
         }
     }
 
-//
-//    @Test
-//    public void save_obj_to_the_db(){
-//        Point point = new Point(1, 1, 1, true);
-//        try (Session session = sessionFactory.openSession()) {
-//            session.beginTransaction();
-//
-//            //
-//            session.persist(point);
-//
-//
-//            session.getTransaction().commit();
-//        }
-//    }
+
     @Test
-    public void clear_all() {
+    public void addPoint(){
+        Point point = new Point(1, 1, 1, true);
         try (Session session = sessionFactory.openSession()) {
             session.beginTransaction();
-
-//            var query = session.createQuery("delete from Point");
-            var query = session.createQuery("delete from Point");
-            query.executeUpdate();
-
+            session.persist(point);
             session.getTransaction().commit();
         }
     }
 
+    @Test
+    public void getPoints() {
+//        try {
+//            TypedQuery<Point> query = em.createQuery("select p from Point p", Point.class);
+//            return query.getResultList();
+//        } catch (Exception e) {
+//            throw e; }
+
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            TypedQuery<Point> query = session.createQuery("from Point", Point.class);
+            for (Point point : query.getResultList()) {
+                System.out.println(point);
+            }
+        }
+    }
+//    @Test
+//    public void clear_all() {
+//        try (Session session = sessionFactory.openSession()) {
+//            session.beginTransaction();
+//
+////            var query = session.createQuery("delete from Point");
+//            var query = session.createQuery("delete from Point");
+//            query.executeUpdate();
+//
+//            session.getTransaction().commit();
+//        }
+//    }
 
 
 //    private static PointDao pointDao;
