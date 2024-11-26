a14b31649e85628e0127cbf63fa6e8eb7316d50e
Kostya666 <eliseevkonstantin306@gmail.com>
2024-11-26 19:45:06 +0300

    add redirect button

--- web/lab4/frontend/src/components/StartPage.js
+++ web/lab4/frontend/src/components/StartPage.js
@@ -2,20 +2,26 @@ import React from 'react';
 import Header from "./Header";
 import {useNavigate} from "react-router-dom";
 import MainPage from "./MainPage";
-import {Link} from "@mui/material";
+import {Button, Link} from "@mui/material";
 
 const StartPage = () => {
 
     const navigate = useNavigate();
 
-    const handleButtonClick = (e) => {
+    const handleButtonClick = () => {
         navigate('/main')
     }
 
     return (
         <div>
             <Header />
-            <button onClick={handleButtonClick}>На основную</button>
+            <Button
+                onClick={handleButtonClick}
+                color="primary"
+                component={Link}
+                to="main"
+            >На основную</Button>
+
         </div>
     )
 }
